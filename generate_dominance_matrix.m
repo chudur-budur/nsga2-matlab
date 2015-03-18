5ed6a787ae069565cd23368fe08ef45758912a96
chudur-budur <khaled.ahsan.talukder@gmail.com>
2015-03-18 13:22:14 -0400

    yo

--- generate_dominance_matrix.m
+++ generate_dominance_matrix.m
@@ -26,19 +26,19 @@ cv_col = nreal+nobj+ncon+1;
 % collect the relevant column data from input array
 cv = pop(:,cv_col);
 objmat = pop(:,obj_col);
-pprint('objmat:\n', objmat);
-pprint('cv:\n', cv);
+% pprint('objmat:\n', objmat);
+% pprint('cv:\n', cv);
 
 % logical array corrresponding to starting IF conditional statement
 ifcvltzero = bsxfun(@and, cv < 0, cv.' < 0);
-pprint('ifcvltzero:\n', ifcvltzero);
+% pprint('ifcvltzero:\n', ifcvltzero);
 
 % create logical arrays corresponding to the three IF-ElSE parts
 ifcvgtlt = bsxfun(@gt, cv, cv.') - bsxfun(@lt, cv, cv.');
-pprint('ifcvgtlt\n', ifcvgtlt);
+% pprint('ifcvgtlt\n', ifcvgtlt);
 ifcvlteq = -bsxfun(@and, cv < 0, cv.' == 0) +...
                     bsxfun(@and, cv == 0, cv.' < 0);
-pprint('ifcvlteq\n', ifcvlteq);
+% pprint('ifcvlteq\n', ifcvlteq);
 
 objcomp = -all(bsxfun(@gt,permute(objmat,[1 3 2]),permute(objmat,[3 1 2])),3) + ...
              all(bsxfun(@lt,permute(objmat,[1 3 2]),permute(objmat,[3 1 2])),3);
@@ -55,24 +55,24 @@ objcomp = -all(bsxfun(@gt,permute(objmat,[1 3 2]),permute(objmat,[3 1 2])),3) + 
 % get the output taking care of all the conditionals
 dom_mat = ifcvltzero .* ifcvgtlt + ...
             (~ifcvltzero) .* (ifcvlteq + (ifcvlteq == 0) .* objcomp);
-pprint('dom_mat:\n', dom_mat);
+% pprint('dom_mat:\n', dom_mat);
 dom_mat = [indices', dom_mat] ;
 
-[nrow,~] = size(objmat);
-dom_mat1 = zeros(nrow,nrow);
-% fill only the upper triangular of mat
-for i = 1:nrow
-    %for j = i + 1:nrow
-    for j = 1:nrow
-        if(i ~= j) 
-            % compare_weights() is a function with some nested 
-            % if-then-else, that compares the values of 1st,2nd and 
-            % 4th columns in data and populates the matrix mat.
-            dom_mat1(i, j) = check_dominance([objmat(i,:), cv(i,:)],...
-                                                [objmat(j,:), cv(j,:)]);
-        end
-    end
-end
-pprint('dom_mat1\n', dom_mat1);
+% [nrow,~] = size(objmat);
+% dom_mat1 = zeros(nrow,nrow);
+% % fill only the upper triangular of mat
+% for i = 1:nrow
+%     %for j = i + 1:nrow
+%     for j = 1:nrow
+%         if(i ~= j) 
+%             % compare_weights() is a function with some nested 
+%             % if-then-else, that compares the values of 1st,2nd and 
+%             % 4th columns in data and populates the matrix mat.
+%             dom_mat1(i, j) = check_dominance([objmat(i,:), cv(i,:)],...
+%                                                 [objmat(j,:), cv(j,:)]);
+%         end
+%     end
+% end
+% pprint('dom_mat1\n', dom_mat1);
 
 end
--- nsga2.m
+++ nsga2.m
@@ -23,28 +23,28 @@ mixed_pop = zeros(2 * popsize, nreal + nobj + ncon + 3);
 % initialize population
 parent_pop = initialize_pop(34654);
 pprint('Initialization done, now performing first generation\n\n');
-pprint('initial pop:\n', parent_pop);
+% pprint('initial pop:\n', parent_pop);
 parent_pop = evaluate_pop(parent_pop, obj_func);
-pprint('initial pop evaluated:\n', parent_pop);
+% pprint('initial pop evaluated:\n', parent_pop);
 parent_pop = assign_rank_and_crowding_distance(parent_pop);
-pprint('initial pop evaluated and ranked:\n', parent_pop);
+% pprint('initial pop evaluated and ranked:\n', parent_pop);
 
 % plot it
-plotpf(1,parent_pop, true);
+plotpf(1,parent_pop, false);
 
-% for i = 2:ngen
-%     fprintf('gen = %d\n', i)
-%     child_pop = selection(parent_pop, child_pop);
-%     child_pop = mutation_pop(child_pop);
-%     % pprint(sprintf('[main gen = %d] child pop before eval:\n', i), child_pop);
-%     child_pop = evaluate_pop(child_pop, obj_func);
-%     % pprint(sprintf('[main gen = %d] child pop evaluated:\n', i), child_pop);
-%     mixed_pop = merge(parent_pop, child_pop);
-%     % pprint(sprintf('[main gen = %d] mixed_pop:\n',i), mixed_pop);
-%     parent_pop = fill_nondominated_sort(mixed_pop);
-%     % pprint(sprintf('[main gen = %d] new parent_pop:\n',i), parent_pop);
-%     
-%     % plot them
-%     plotpf(i, parent_pop);    
-% end
+for i = 2:ngen
+    fprintf('gen = %d\n', i)
+    child_pop = selection(parent_pop, child_pop);
+    child_pop = mutation_pop(child_pop);
+    % pprint(sprintf('[main gen = %d] child pop before eval:\n', i), child_pop);
+    child_pop = evaluate_pop(child_pop, obj_func);
+    % pprint(sprintf('[main gen = %d] child pop evaluated:\n', i), child_pop);
+    mixed_pop = merge(parent_pop, child_pop);
+    % pprint(sprintf('[main gen = %d] mixed_pop:\n',i), mixed_pop);
+    parent_pop = fill_nondominated_sort(mixed_pop);
+    % pprint(sprintf('[main gen = %d] new parent_pop:\n',i), parent_pop);
+    
+    % plot them
+    plotpf(i, parent_pop);    
+end
 pprint('done\n');
